d634c53e635c95c79bf90a777cd9ec304c91ad92
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-03 20:58:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,12 +8,22 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+<<<<<<< HEAD
   #pp directors_database
   element_index = 0
   titles = directors_database[0][:movies] 
   while element_index < titles.count do
       puts titles[element_index][:title]
   
+=======
+  pp directors_database
+  element_index = 0
+  titles = directors_database[0][:movies] 
+  while element_index < titles do
+     #if directors_database[element_index] == Spielberg 
+      #p directors_database[element_index]
+    #end
+>>>>>>> 7bd1960c24d803ced58387580c4db18746e9f80a
     element_index += 1
   end
 end
